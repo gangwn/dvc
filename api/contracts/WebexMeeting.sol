aadb6564904be43afa40ba16529684ef9debbafc
bingyy99 <42613992+bingyy99@users.noreply.github.com>
2018-08-27 22:19:59 +0800

    Update WebexMeeting.sol
    Update interface

--- api/contracts/WebexMeeting.sol
+++ api/contracts/WebexMeeting.sol
@@ -1,45 +1,67 @@
 pragma solidity ^0.4.23;
 
 contract WebexMeeting {
-  string public mMeetingKey;
-  string mMeetingSite;
-  string mMeetingTopic;
-  uint mDuration;
-  uint256 mStartTime;
+  //struct User {
+  //  address addr;
+  //  uint amount;
+  //  uint userId;
+  //  string token;
+  //  string displayName;
+  //}
 
-  event MeetingStarted(string confId);
+  struct Meeting {
+    address addr;  // the meeting host address
+    string topic;
+    uint256 startTime;
+    uint duration;
+    string meetingKey;
+    uint128 confId;
+    string invitees;
+  }
+
+
+  mapping(address => Meeting[]) public meetings;
 
   function startMeeting() public returns (string) {
     return ("Start Meeting");
   }
 
-  function scheduleMeeting(string meetingtopic, uint256 starttime, uint duration) public returns (string) {
-    require (!stringIsNull(meetingtopic), "meeting topic is null");
-    mMeetingTopic = meetingtopic;
+  function scheduleMeeting(string topic, uint256 starttime, uint duration, string invites) public returns (string) {
+    require (!stringIsNull(topic), "meeting topic is null");
 
     require (duration > 0, "meeting duration is must > 0");
-    mDuration = duration;
 
     require (starttime > 0, "meeting starttime is must > 0");
-    mStartTime = starttime;
 
-    "Log";
     gasleft();
     msg.sender;
     msg.data;
     gasleft();
 
-    mMeetingKey = "348397514";
-    mMeetingSite = "go.webex.com";
-    return mMeetingKey;
+    Meeting memory meeting = Meeting(msg.sender, topic, starttime, duration, "348397514", 1231231232123, "");
+    Meeting[] myMeetings = meetings[msg.sender];
+    myMeetings[myMeetings.length++] = meeting;
+    return meeting.meetingKey;
   }
 
-  function getMeetingKey() public returns (string) {
-    return string(mMeetingKey);
-  }
+  function getUpcomingMeeting() public returns (string){
+    address addr = msg.sender;
+    Meeting[] storage myMeetings = meetings[addr];
 
-  function getMeetingSite() public returns (string) {
-    return string(mMeetingSite);
+    if(myMeetings.length == 0){
+      return "";
+    }
+
+    uint256 nearByStartTime = 999999999999999999999999;
+    uint upcomingIndex = 0;
+    for (uint i = 0; i < myMeetings.length; i++) {
+      if(nearByStartTime > myMeetings[i].startTime){
+         upcomingIndex = i;
+         nearByStartTime = myMeetings[i].startTime;
+      }
+
+      return myMeetings[upcomingIndex].meetingKey;
+    }
   }
 
   function stringIsNull(string str) private returns (bool) {
@@ -50,5 +72,4 @@ contract WebexMeeting {
         return false;
       }
   }
-
 }
