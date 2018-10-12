bcd1e97c906fc42a6d43004e17b804ec96730c47
bingyy99 <42613992+bingyy99@users.noreply.github.com>
2018-10-12 15:18:07 +0800

    Update 2_deploy_contracts.js

--- api/contract/migrations/2_deploy_contracts.js
+++ api/contract/migrations/2_deploy_contracts.js
@@ -4,6 +4,7 @@ var ServiceManager = artifacts.require("./ServiceManager.sol");
 var Utils = artifacts.require("./Utils.sol");
 var ConferenceService = artifacts.require("./ConferenceService.sol");
 var CCSService = artifacts.require("./CCSService.sol");
+var Token = artifacts.require("./FixedSupplyToken.sol");
 module.exports = function(deployer) {
     deployer.then(async () => {
         deployer.logger.log("Start to deploy contracts")
@@ -14,7 +15,9 @@ module.exports = function(deployer) {
 
         await deployer.deploy(Utils);
         await deployer.link(Utils, ConferenceService);
+        await deployer.deploy(Token);
 
+        await deployHelper.deployContract(Token, "FixedSupplyToken")
         await deployHelper.deployContract(ConferenceService, "ConferenceService", serviceManager.address)
 
         await deployHelper.deployContract(CCSService, "CCSService", serviceManager.address)
